a63ef3e5fd7f47df063cd422f323d6983665e5f8
Stefan Scheffler <sscheffler@avantgarde-labs.de>
2016-04-03 09:39:09 +0200

    course5.week1.assignment

---
+++ course5/node-express/server.js
@@ -0,0 +1,20 @@
+/**
+ * Used for assignments
+ */
+var dishRouter = require('./dishRouter'),
+    promoRouter = require('./promoRouter'),
+    leadershipRouter = require('./leadershipRouter'),
+    express = require('express'),
+    morgan = require('morgan');
+var serverCredentials = {host: 'localhost', port: 3000};
+var app = express();
+
+app.use(morgan('dev'))
+    .use(express.static(__dirname + '/public'))
+    .use('/dishes',dishRouter)
+    .use('/promotions',promoRouter)
+    .use('/leadership',leadershipRouter)
+    .listen(serverCredentials.port, serverCredentials.host, function(){
+        console.log(`Server running at http://${serverCredentials.host}:${serverCredentials.port}/`);
+    });
+
